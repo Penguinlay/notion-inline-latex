b965fa191fc00267ea4bd7c263d4b2316b3d13f5
Penguinlay <13214144+Penguinlay@users.noreply.github.com>
2019-08-15 23:06:55 -0700

    v0.4: Fix Cursor Displacement; Apply prettier

--- notion-inline-latex.js
+++ notion-inline-latex.js
@@ -2,7 +2,7 @@
 // @name            Notion Inline LaTeX
 // @namespace       https://github.com/Penguinlay/notion-inline-latex
 // @match           https://www.notion.so/*
-// @version         0.3
+// @version         0.4
 // @description     User Script for Inline LaTeX Rendering in notion.so
 // @require         https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.js
 // @require         https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/contrib/auto-render.min.js
@@ -25,25 +25,32 @@ GM_addStyle(`
 .katex {
     padding-right: 0 !important;
 }
-`)
+`);
 
 // declare/init vars
-let timer;                              // timer identifier
-let startUpWaitTime = 3000;             // ms after startup (adjust as needed)
-let keyUpWaitTime   = 2000;             // ms after keyup   (adjust as needed)
+let timer; // timer identifier
+let startUpWaitTime = 3000; // ms after startup (adjust as needed)
+let keyUpWaitTime = 2000; // ms after keyup   (adjust as needed)
 
 // render inline LaTeX
 function renderInlineLaTeX() {
-    renderMathInElement(document.body, {
-        delimiters: [
-            // LaTeX delimiters (uncomment/add as needed)
-            // { left: "$$" , right: "$$" , display: true  },
-            // { left: "\\[", right: "\\]", display: true  },
-            // { left: "\\(", right: "\\)", display: false },
-            { left: "$"  , right: "$"  , display: false }
-        ]
-    });
-    console.log("Inline LaTeX is rendered.");
+  renderMathInElement(document.body, {
+    delimiters: [
+      // LaTeX delimiters (uncomment/add as needed)
+      // { left: "$$" , right: "$$" , display: true  },
+      // { left: "\\[", right: "\\]", display: true  },
+      // { left: "\\(", right: "\\)", display: false },
+      { left: "$", right: "$", display: false }
+    ]
+  });
+  enterEscapeKey();
+  console.log("Inline LaTeX is rendered.");
+}
+
+// select block to avoid cursor displacement
+function enterEscapeKey() {
+  var keyEvent = new KeyboardEvent("keypress", { key: "Escape" });
+  document.body.dispatchEvent(keyEvent);
 }
 
 // on startup...
@@ -53,7 +60,7 @@ timer = setTimeout(renderInlineLaTeX, startUpWaitTime);
 
 // on keyup...
 document.addEventListener("keyup", () => {
-    console.log("Rendering inline LaTeX on keyup...");
-    clearTimeout(timer);
-    timer = setTimeout(renderInlineLaTeX, keyUpWaitTime);
+  console.log("Rendering inline LaTeX on keyup...");
+  clearTimeout(timer);
+  timer = setTimeout(renderInlineLaTeX, keyUpWaitTime);
 });
